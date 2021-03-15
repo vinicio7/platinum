a43d5189e6fc0810aca3ced6b687f3b8e0a4bac0
Vinicio <vjlopezdeleon@gmail.com>
2021-03-14 22:06:03 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en-US" class="scheme_original">
    <head>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
       <meta name="format-detection" content="telephone=no">
